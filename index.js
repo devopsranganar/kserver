4f30ed44ac5019490983cfe7a6ac8f3178877eb1
devopsranganar <devopsrangnar@gmail.com>
2020-03-06 13:00:09 +0530

    cros

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+var cors = require('cors');
  
 const Schema = mongoose.Schema;
 const hash = new Schema({
@@ -12,7 +12,7 @@ const hash = new Schema({
 const Hash =  mongoose.model('Hash', hash);
 const PORT =3000;
 const app = express();
-
+app.use(cors());
 mongoose.connect('mongodb+srv://devops:rq6ve2dr@cluster0-40smv.mongodb.net/test?retryWrites=true&w=majority', () =>{console.log('conected to DB')});
 app.use(bodyParser());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,11 +23,7 @@ console.log(req.params.valueId);
   const getX = await Hash.findOne({id:req.params.valueId}).then((result) =>{ return result.toJSON();})
 console.log(getX.value);
    if(getX) return res.send(getX.value.toString());
-  
-
-
-
-});
+  });
 
 app.get('/', (req,res) => {
 res.send('Hello Word!');
