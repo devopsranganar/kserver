5163d82391bf553fd99fb0496d95ae6ce2578d31
devopsranganar <devopsrangnar@gmail.com>
2020-03-07 10:40:45 +0530

    disabled mongo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 var cors = require('cors');
 const https = require('https');
 const fs = require('fs')
@@ -12,28 +12,34 @@ var options = {
   cert: cert
 };
 
-const Schema = mongoose.Schema;
-const hash = new Schema({
-    id: { type: String   },
-    hash: { type: String },
-    value:{type:Number}
-});
-const Hash =  mongoose.model('Hash', hash);
+//const Schema = mongoose.Schema;
+//const hash = new Schema({
+//    id: { type: String   },
+ //   hash: { type: String },
+ //   value:{type:Number}
+//});
+//const Hash =  mongoose.model('Hash', hash);
+
 const PORT =3000;
 const app = express();
+
 app.use(cors());
-mongoose.connect('mongodb+srv://devops:rq6ve2dr@cluster0-40smv.mongodb.net/test?retryWrites=true&w=majority', () =>{console.log('conected to DB')});
+
+//mongoose.connect('mongodb+srv://devops:rq6ve2dr@cluster0-40smv.mongodb.net/test?retryWrites=true&w=majority', () =>{console.log('conected to DB')});
+
+
 app.use(bodyParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/xvalue/:valueId', async (req,res) => {
 console.log(req.params.valueId);
-  const getX = await Hash.findOne({id:req.params.valueId}).then((result) =>{ return result.toJSON();})
-console.log(getX.value);
-   if(getX) return res.send(getX.value.toString());
+ // const getX = await Hash.findOne({id:req.params.valueId}).then((result) =>{ return result.toJSON();})
+var getx = req.params.valueId.toString()+1;
+console.log(getx);
+   if(getx) return res.send(getx);
   });
-
+  
 app.get('/', (req,res) => {
 res.send('Hello Word!');
 });
